d777868c2696bffb66f57fb17f4566ff3c8ad919
Megan <okerlund.megan@gmail.com>
2019-12-17 21:49:55 -0800

    Made small changes

--- webpage/Html_Code/styles.js
+++ webpage/Html_Code/styles.js
@@ -1,17 +1,17 @@
-// var tooltipTimeout;
+var tooltipTimeout;
 
-// $(".infotab").hover(function()
-//                     {tooltipTimeout = setTimeout(showTooltip, 200);}, 
-//                     hideTooltip);
+$(".infotab").hover(function()
+                    {tooltipTimeout = setTimeout(showTooltip, 200);}, 
+                    hideTooltip);
 
-// function showTooltip()
-//     {
-//     var tooltip = $("<div id='tooltip' class='tooltip'>I'm the tooltip!</div>");
-//     tooltip.appendTo($(".infotab"));
-//     }
+function showTooltip()
+    {
+    var tooltip = $("<div id='tooltip' class='tooltip'>I'm the tooltip!</div>");
+    tooltip.appendTo($(".infotab"));
+    }
 
-// function hideTooltip()
-//     {
-//     clearTimeout(tooltipTimeout);
-//     $("#tooltip").fadeOut().remove();
-//     }
+function hideTooltip()
+    {
+    clearTimeout(tooltipTimeout);
+    $("#tooltip").fadeOut().remove();
+    }
