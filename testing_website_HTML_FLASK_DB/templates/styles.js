f81aecac6e62bbb78c50befab6cdfdad6f9be72e
Romina-R <romina@riener.me>
2019-12-17 22:54:56 -0800

    updated testing_website and flask app, routes to each html page working

---
+++ testing_website_HTML_FLASK_DB/templates/styles.js
@@ -0,0 +1,17 @@
+// var tooltipTimeout;
+
+// $(".infotab").hover(function()
+//                     {tooltipTimeout = setTimeout(showTooltip, 200);}, 
+//                     hideTooltip);
+
+// function showTooltip()
+//     {
+//     var tooltip = $("<div id='tooltip' class='tooltip'>I'm the tooltip!</div>");
+//     tooltip.appendTo($(".infotab"));
+//     }
+
+// function hideTooltip()
+//     {
+//     clearTimeout(tooltipTimeout);
+//     $("#tooltip").fadeOut().remove();
+//     }
